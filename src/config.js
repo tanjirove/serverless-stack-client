9d9ce7be313c865a6976cb6e8df65bab398fcf6e
Tanjir Ahmed <tanjir@faqinteractive.com.au>
2020-08-18 17:36:35 +0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "faq-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://wgfgx3cxl1.execute-api.ap-southeast-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_Ubyp411Jw",
+    APP_CLIENT_ID: "29052qnnov8uoavc62dea0n05c",
+    IDENTITY_POOL_ID: "ap-southeast-2:4ab4d693-7481-4f4e-94b5-668179c0d2a7",
+  },
+};
